12f98273b8f8989ac2bc97d1716e728be1d92722
gs298 <shrey.om11@gmail.com>
2015-04-27 20:14:18 -0400

    Delete fuction Response handler

---
+++ BookInventoryManagement/ScanBuy/src/com/scanbuy/ResponseHandler/DeleteResponse.java
@@ -0,0 +1,19 @@
+package com.scanbuy.ResponseHandler;
+
+public class DeleteResponse {
+	
+	String statusMessage;
+	String respMessage;
+	public String getRespMessage() {
+		return respMessage;
+	}
+	public void setRespMessage(String respMessage) {
+		this.respMessage = respMessage;
+	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+}
